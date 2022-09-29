ec32a9470edecc22dd006d15cca5fad7dcf392ff
Ivan Servacek <servacek.ivan@gmail.com>
2022-09-29 14:45:08 +0200

    Initial Commit

---
+++ donothing/donothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  donothingApp.swift
+//  donothing
+//
+//  Created by Ivan Servacek on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct donothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
